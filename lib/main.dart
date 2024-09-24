45674b9f2737120e381d89e5b2d76b7f5be53f8c
myounis007 <myounis4707@gmail.com>
2024-09-24 10:35:33 +0500

    ssff

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
-
 import 'app_router/app_router.dart';
 
 import 'theme/app_colors/app_colors.dart';
-import 'view/add_medicine/add_detail_two_view';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
